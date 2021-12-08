16e1b3ea8432a9698f9456eccedc0c11bcf95dcd
picunada <xlaystgoku@gmail.com>
2021-12-08 15:43:08 +0300

    Created API

---
+++ Repositories/IItemsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.Dtos;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Item GetItem(Guid id);
+        IEnumerable<Item> GetItems();
+        void CreateItem(Item item);
+        void UpdateItem(Item item);
+        void DeleteItem(Guid id);
+    }
+}
