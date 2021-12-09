0d0af8414d8801b74daab6ea51a64fd1d91bbfae
picunada <xlaystgoku@gmail.com>
2021-12-09 11:13:45 +0300

    Make service async

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Catalog.Dtos;
 using Catalog.Entities;
 
@@ -7,10 +8,10 @@ namespace Catalog.Repositories
 {
     public interface IItemsRepository
     {
-        Item GetItem(Guid id);
-        IEnumerable<Item> GetItems();
-        void CreateItem(Item item);
-        void UpdateItem(Item item);
-        void DeleteItem(Guid id);
+        Task<Item> GetItemAsync(Guid id);
+        Task<IEnumerable<Item>> GetItemsAsync();
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
     }
 }
